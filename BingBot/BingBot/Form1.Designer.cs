02b94fc29f483e73f60b4c76fd81e8bbad003a55
BuildTools <kurtisbowen99@gmail.com>
2015-11-25 18:09:01 -0600

    Code Actually Does Stuff

--- BingBot/BingBot/Form1.Designer.cs
+++ BingBot/BingBot/Form1.Designer.cs
@@ -28,9 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Web_Bing = new System.Windows.Forms.WebBrowser();
             this.Prg_Main = new System.Windows.Forms.ProgressBar();
             this.Btn_Start = new System.Windows.Forms.Button();
+            this.Tmr_Run = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // Web_Bing
@@ -38,36 +40,46 @@
             this.Web_Bing.Location = new System.Drawing.Point(12, 41);
             this.Web_Bing.MinimumSize = new System.Drawing.Size(20, 20);
             this.Web_Bing.Name = "Web_Bing";
-            this.Web_Bing.Size = new System.Drawing.Size(984, 333);
+            this.Web_Bing.Size = new System.Drawing.Size(1160, 333);
             this.Web_Bing.TabIndex = 0;
             // 
             // Prg_Main
             // 
             this.Prg_Main.Location = new System.Drawing.Point(12, 12);
+            this.Prg_Main.Maximum = 30;
             this.Prg_Main.Name = "Prg_Main";
-            this.Prg_Main.Size = new System.Drawing.Size(984, 23);
+            this.Prg_Main.Size = new System.Drawing.Size(1160, 23);
             this.Prg_Main.TabIndex = 1;
             // 
             // Btn_Start
             // 
             this.Btn_Start.Location = new System.Drawing.Point(12, 380);
             this.Btn_Start.Name = "Btn_Start";
-            this.Btn_Start.Size = new System.Drawing.Size(984, 49);
+            this.Btn_Start.Size = new System.Drawing.Size(1160, 49);
             this.Btn_Start.TabIndex = 2;
-            this.Btn_Start.Text = "button1";
+            this.Btn_Start.Text = "Start";
             this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            // 
+            // Tmr_Run
+            // 
+            this.Tmr_Run.Interval = 5000;
+            this.Tmr_Run.Tick += new System.EventHandler(this.Tmr_Run_Tick);
             // 
             // Frm_Bot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 441);
+            this.ClientSize = new System.Drawing.Size(1184, 441);
             this.Controls.Add(this.Btn_Start);
             this.Controls.Add(this.Prg_Main);
             this.Controls.Add(this.Web_Bing);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Frm_Bot";
             this.Text = "Bing Bot";
+            this.Load += new System.EventHandler(this.Frm_Bot_Load);
             this.ResumeLayout(false);
 
         }
@@ -77,6 +89,7 @@
         private System.Windows.Forms.WebBrowser Web_Bing;
         private System.Windows.Forms.ProgressBar Prg_Main;
         private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Timer Tmr_Run;
     }
 }
 
--- BingBot/BingBot/Form1.cs
+++ BingBot/BingBot/Form1.cs
@@ -12,9 +12,110 @@ namespace BingBot
 {
     public partial class Frm_Bot : Form
     {
+        int Searches = 0;
+        Random rndGen = new Random();
+
         public Frm_Bot()
         {
             InitializeComponent();
+        }
+
+        private void Frm_Bot_Load(object sender, EventArgs e)
+        {
+            Web_Bing.Navigate("http://www.bing.com/");
+            MessageBox.Show("Please Sign Into Bing Rewards (User Info is Dumped After Program Completes)");
+        }
+
+        private void Btn_Start_Click(object sender, EventArgs e)
+        {
+            if (Btn_Start.Text == "Start")
+            {
+                MessageBox.Show("WARNING: This program assumes you are signed into bing within the web element, and will only work if you are", "Warning!");
+                Tmr_Run.Start();
+                Btn_Start.Text = "Pause";
+            } else if (Btn_Start.Text == "Manual Point Earning")
+            {
+                Web_Bing.Navigate("https://www.bing.com/rewards/dashboard");
+            } else if (Btn_Start.Text == "Pause")
+            {
+                Tmr_Run.Enabled = !Tmr_Run.Enabled;
+            }
+        }
+
+        private void Tmr_Run_Tick(object sender, EventArgs e)
+        {
+            if (Searches < 31)
+            {
+                try
+                {
+                    String webLink = "http://www.bing.com/search?q=" + RandySearch();
+                    Web_Bing.Navigate(webLink);
+                    Searches++;
+                    Prg_Main.Value = Searches;
+                } catch
+                {
+                    Console.WriteLine("Page Navigation Failed, Retrying");
+                }
+
+            } else
+            {
+                Tmr_Run.Stop();
+                Btn_Start.Text = "Manual Point Earning";
+                MessageBox.Show("Auto Point Earning Complete");
+            }
+        }
+
+        private String RandySearch()
+        {
+            String[] Question =
+            {
+                "How Far Can A",
+                "What is A",
+                "How Do You"
+            };
+
+            String[] Noun =
+            {
+                "Cat",
+                "Car",
+                "Dog",
+                "Person",
+                "Human",
+                "Drill",
+                "Random",
+                "Class",
+                "Code",
+                "Microsoft",
+                "Bing",
+                "Google",
+                "ALIENS",
+                "Game",
+                "SWAG",
+                "Flight",
+                "Airplane"
+            };
+
+            String[] Action =
+            {
+                "Swim",
+                "Die",
+                "Live",
+                "Kill",
+                "Revive",
+                "Play",
+                "Program",
+                "Code",
+                "Fly",
+                "Throw",
+                "Explode",
+                "Explore",
+                "Repair",
+                "Game",
+                "SWAG",
+                "Fire"
+            };
+
+            return Question[rndGen.Next(0, Question.Length)] + " " + Noun[rndGen.Next(0, Noun.Length)] + " " + Action[rndGen.Next(0, Action.Length)];
         }
     }
 }
